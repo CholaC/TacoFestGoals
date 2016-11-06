71de68b3cfb565ef5a651057b7bcaebd8b0845e8
tahani Alharbi <tahani.alharbi.m@gmail.com>
2016-11-06 14:38:22 -0500

    no message

--- Goals/app/src/main/java/com/tacofest/goals/editGrades.java
+++ Goals/app/src/main/java/com/tacofest/goals/editGrades.java
@@ -1,20 +1,33 @@
 package com.tacofest.goals;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class editGrades extends AppCompatActivity {
+
+    Button btnsaveGrade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_grades);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+
+        btnsaveGrade = (Button) findViewById(R.id.btnSave);
+
+        btnsaveGrade.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(editGrades.this, gradeDetiles.class);
+                startActivity(i);
+            }
+        });
+
     }
 
 }
--- Goals/app/src/main/java/com/tacofest/goals/gradeDetiles.java
+++ Goals/app/src/main/java/com/tacofest/goals/gradeDetiles.java
@@ -1,5 +1,6 @@
 package com.tacofest.goals;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,12 +10,21 @@ import android.view.View;
 
 public class gradeDetiles extends AppCompatActivity {
 
+    FloatingActionButton btneditGrades;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade_detiles);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        btneditGrades = (FloatingActionButton) findViewById(R.id.fbtnEditGrades);
+
+        btneditGrades.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(gradeDetiles.this, editGrades.class);
+                startActivity(i);
+            }
+        });
+
 
     }
 
